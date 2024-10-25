bffce422eabb20244dd63dc8582ed20ab0fcfd7a
umdoyuun <tkdtlr1998@naver.com>
2024-10-25 17:52:26 +0900

    121p Timer.c 22까지 완료

---
+++ hal/HalTimer.h
@@ -0,0 +1,6 @@
+#ifndef HAL_HALTIMER_H_
+#define HAL_HALTIMER_H_
+
+void	Hal_timer_init(void);
+
+#endif /* HAL_HALTIMER_H_ */
---
+++ hal/rvpb/Timer.c
@@ -0,0 +1,26 @@
+#include "stdint.h"
+#include "Timer.h"
+#include "HalTimer.h"
+#include "HalInterrupt.h"
+
+extern volatile Timer_t* Timer;
+
+static void interrupt_handler(void);
+
+static uint32_t internal_1ms_counter;
+
+void Hal_timer_init(void)
+{
+	// interface reset
+	Timer->timerxcontrol.bits.TimerEn = 0;
+	Timer->timerxcontrol.bits.TimerMode = 0;
+	Timer->timerxcontrol.bits.OneShot = 0;	
+	Timer->timerxcontrol.bits.TimerSize = 0;
+	Timer->timerxcontrol.bits.TimerPre = 0;
+	Timer->timerxcontrol.bits.IntEnable = 1;
+	Timer->timerxload = 0;
+	Timer->timerxvalue = 0xFFFFFFFF;
+	
+	// set periddic mode
+
+}
